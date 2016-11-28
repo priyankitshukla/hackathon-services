5865030ce0a3589f868f717ec4550e093944d519
aradhya <priyankitshukla@gmail.com>
2016-11-28 16:10:21 +0530

    add

--- src/main/java/com/chat/service/hackathon/SearchService.java
+++ src/main/java/com/chat/service/hackathon/SearchService.java
@@ -1,24 +1,77 @@
 package com.chat.service.hackathon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Hello world!
- * 
- */
+import com.chat.service.hackathon.pojo.SearchRequest;
+import com.chat.service.hackathon.pojo.SearchResponse;
+
 @SpringBootApplication
 @RestController
 public class SearchService {
 	
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String getSearchService(){
+	@RequestMapping(value="/searchservice", method=RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody List<SearchResponse> getSearchService(@RequestBody SearchRequest searchRequest){
+		
+		List<SearchResponse> searchResponseList = new ArrayList<SearchResponse>();
+		if(StringUtils.equalsIgnoreCase("searchFid.com",searchRequest.getResult().getMetadata().getIntentName())){
+			String url = "https://search.fidelity.com/search/getSearchResults?question="+searchRequest.getResult().getParameters().getProduct_service().get(0);
+			try {
+				HttpClient client = HttpClientBuilder
+						.create().build();
+				HttpGet request = new HttpGet(url);
+				request.addHeader("User-Agent",
+						"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0");
 	
-		return "it works!";
+				HttpResponse response = client
+						.execute(request);
+				BufferedReader bufferedReader = new BufferedReader(
+						new InputStreamReader(response
+								.getEntity().getContent()));
+				StringBuffer result = new StringBuffer();
+				String line = "";
+				while ((line = bufferedReader.readLine()) != null) {
+					result.append(line);
+	
+				}
+				String resultol = StringUtils
+						.substringBetween(
+								result.toString(), "<ol>",
+								"</ol>");
+				Document document = Jsoup.parse(resultol);
+				Elements links = document.select("a[href]");
+				for (Element link : links) {
+					SearchResponse searchResponse = new SearchResponse();
+					searchResponse.setSpeech(link.text());
+					searchResponse.setData(link.attr("href"));
+					searchResponseList.add(searchResponse);
+				}
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		}
+		return searchResponseList;
 	}
 	public static void main(String[] args){
 		SpringApplication.run(SearchService.class, args);
